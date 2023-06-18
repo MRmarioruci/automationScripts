749b8ed7576a2134464281198bd8419326e07b1a
Mario Ruci <marioruci15@gmail.com>
2023-06-18 13:21:31 +0300

    deploy ec2 first version

---
+++ main.py
@@ -0,0 +1,64 @@
+import paramiko
+import subprocess
+
+# SSH connection parameters
+hostname = '16.170.7.88'
+port = 22
+username = 'ubuntu'
+private_key_path = '/home/mario/.ssh/havenojob.pem'
+
+# Create an SSH client
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+local_folder_path = '../havenojob'
+excluded_local_folder_path = '../havenojob/node_modules'
+remote_folder = '~/havenojob'
+remote_folder_path = f'{username}@{hostname}:~/'
+excluded_items = ['node_modules/', 'client/node_modules/', '.git/']
+rsync_args = ['-av', '-e', f'ssh -i {private_key_path}']
+
+try:
+
+    private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+
+    # Connect to the SSH server
+    ssh.connect(hostname, port, username, pkey=private_key)
+
+    # Install Docker
+    stdin, stdout, stderr = ssh.exec_command('sudo apt update')
+    print(stdout.read().decode())
+    stdin, stdout, stderr = ssh.exec_command('sudo apt install docker.io -y')
+    print(stdout.read().decode())
+    stdin, stdout, stderr = ssh.exec_command('sudo systemctl start docker')
+    print(stdout.read().decode()) 
+    stdin, stdout, stderr = ssh.exec_command('sudo systemctl enable docker')
+    print(stdout.read().decode()) 
+
+    # Install Docker Compose
+    stdin, stdout, stderr = ssh.exec_command('sudo apt install curl -y')
+    print(stdout.read().decode())
+    stdin, stdout, stderr = ssh.exec_command('sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose')
+    print(stdout.read().decode())
+    stdin, stdout, stderr = ssh.exec_command('sudo chmod +x /usr/local/bin/docker-compose')
+    print(stdout.read().decode())
+
+    # Verify Docker and Docker Compose installations
+    stdin, stdout, stderr = ssh.exec_command('docker --version')
+    print(stdout.read().decode())
+    stdin, stdout, stderr = ssh.exec_command('docker-compose --version')
+    print(stdout.read().decode())
+
+    # Upload files
+    for item in excluded_items:
+        rsync_args.extend(['--exclude', item])
+
+    rsync_args.extend([local_folder_path, remote_folder_path])
+    subprocess.run(['rsync'] + rsync_args)
+
+    # Start the containers ~ This will take a bit
+    stdin, stdout, stderr = ssh.exec_command(f'cd ./havenojob && sudo docker-compose up')
+    print(stdout.read().decode())
+
+finally:
+    ssh.close()
